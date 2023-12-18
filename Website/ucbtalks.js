046b9567929396b3016db4d47c8d66c676a74621
Ajaxculious <151891110+Ajaxculious@users.noreply.github.com>
2023-12-18 12:20:09 +0800

    Update ucbtalks.js

--- Website/ucbtalks.js
+++ Website/ucbtalks.js
@@ -1,1 +1,18 @@
-//hello
+function education() {
+    document.body.classList.toggle("educ");
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    }
+
+    function homed() {
+        document.body.classList.toggle("hom");
+        document.getElementById('bgimg').style.display = 'block';
+        document.getElementById('bigass').style.display = 'block';
+        }
+    
+function homed() {
+            document.body.classList.toggle("hom");
+            document.getElementById('bgimg').style.display = 'block';
+            document.getElementById('bigass').style.display = 'block';
+            }
+    
