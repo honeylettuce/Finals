85023de74e7f9ed798bc80bbc2d61cbe2110a912
honeylettuce <143990919+honeylettuce@users.noreply.github.com>
2023-12-20 17:11:59 +0800

    Update ucbtalks.js

--- Website/ucbtalks.js
+++ Website/ucbtalks.js
@@ -2,20 +2,22 @@ function homed() {
     document.body.classList.toggle("hom");
     document.getElementById('bgimg').style.display = 'block';
     document.getElementById('bigass').style.display = 'block';
+    document.getElementById('edu').style.display = 'none';
     }
-    
-    function education() {
+
+function education() {
     document.body.classList.toggle("educ");
     document.getElementById('bgimg').style.display = 'none';
     document.getElementById('bigass').style.display = 'none';
+    document.getElementById('posts').style.display= 'none';
     }
-
+    
 function story() {
     document.body.classList.toggle("stor");
     document.getElementById('bgimg').style.display = 'none';
     document.getElementById('bigass').style.display = 'none';
     }
-
+    
 function about() {
     document.body.classList.toggle("abou");
     document.getElementById('bgimg').style.display = 'none';
